97cac399de3799235a0e6d293a537146c9066fb5
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-04-16 12:19:50 -0400

    adding account updating features.

--- flaskapp/flaskapp/routes.py
+++ flaskapp/flaskapp/routes.py
@@ -1,6 +1,8 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from flaskapp import app, db, bcrypt
-from flaskapp.forms import RegistrationForm, LoginForm
+from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -65,7 +67,29 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    rand_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.profile_pic = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', category='success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    img_file = url_for('static', filename='profile_pics/' + current_user.profile_pic)
+    return render_template('account.html', title='Account', img_file=img_file, form=form)
