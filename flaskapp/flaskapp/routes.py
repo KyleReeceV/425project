2fd66fceaac34c3ace6eef1bc51bf2d33a8ccb02
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-04-16 12:39:56 -0400

    added forced image size

--- flaskapp/flaskapp/routes.py
+++ flaskapp/flaskapp/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskapp import app, db, bcrypt
 from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -70,9 +71,15 @@ def logout():
 def save_picture(form_picture):
     rand_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
+    picture_fn = rand_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    #force image sizes
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
     return picture_fn
 
 @app.route("/account", methods=['GET', 'POST'])
