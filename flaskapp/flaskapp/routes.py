5a3fe1fd5a753b9e9ef6560afb3d0f85577c6d60
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-04-19 20:00:49 -0400

    forgot quotes

--- flaskapp/flaskapp/routes.py
+++ flaskapp/flaskapp/routes.py
@@ -170,7 +170,7 @@ def reset_token(token):
     user = User.verify_reset_token(token)
     if user is None:
         flash('That is an invalid or expired token.', 'warning')
-        return redirect(url_for(reset_request))
+        return redirect(url_for('reset_request'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
