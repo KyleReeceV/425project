96e33775c5e606a950f9f142d9d5be30058e1665
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-04-27 14:26:35 -0400

    undoing app instance changes bc/ host problems

--- flaskapp/flaskapp/__init__.py
+++ flaskapp/flaskapp/__init__.py
@@ -5,28 +5,23 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskapp.config import Config
 
-#-Extensions
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail()
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
+mail = Mail(app)
 
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    mail.init_app(app)
+from flaskapp.users.routes import users
+from flaskapp.posts.routes import posts
+from flaskapp.main.routes import main
 
-    from flaskapp.users.routes import users
-    from flaskapp.posts.routes import posts
-    from flaskapp.main.routes import main
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
-
-    return app
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
--- flaskapp/run.py
+++ flaskapp/run.py
@@ -1,6 +1,4 @@
-from flaskapp import create_app
-
-app = create_app()
+from flaskapp import app
 
 if __name__ == '__main__':
     app.run(debug=True)
