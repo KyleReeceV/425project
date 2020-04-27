4a5149f87e2d864073348746829edef3742c36e3
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-04-27 14:09:30 -0400

    redoing app structure for multi-instance

--- flaskapp/flaskapp/users/utils.py
+++ flaskapp/flaskapp/users/utils.py
@@ -1,15 +1,15 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for
+from flask import url_for, current_app
 from flask_mail import Message
-from flaskapp import app, mail
+from flaskapp import mail
 
 def save_picture(form_picture):
     rand_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = rand_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     #force image sizes
     output_size = (125, 125)
